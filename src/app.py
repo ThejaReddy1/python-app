0d926e9f71fd7f51fa866bfdca1a78bdfe2a2380
theja reddy <gundlooruthejareddy@gmail.com>
2026-08-25 05:32:18 +0000

    Adds ci github workflow

--- src/app.py
+++ src/app.py
@@ -12,6 +12,7 @@ def get_details():
         'hostname': socket.gethostname(),
         'timestamp': datetime.datetime.now().strftime("%I:%M:%S %p on %B %d, %Y"),
         'version': '1.0.0',
+        'message': 'You are doing great, human!',
         'description': 'This is a sample API for demonstration purposes.'
     }
     return jsonify(details)
