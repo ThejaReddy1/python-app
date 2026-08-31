c9d131f0ca5543ba68c0dc3ec4482f6d5e27cc38
theja reddy <gundlooruthejareddy@gmail.com>
2026-08-31 05:13:48 +0000

    Adds new info endpoint

--- src/app.py
+++ src/app.py
@@ -6,24 +6,24 @@ import datetime, socket, os
 
 app = Flask(__name__)
 
-@app.route('/api/v1/details', methods=['GET'])
-def get_details():
-    details = {
+@app.route('/api/v1/info', methods=['GET'])
+def get_info():
+    return jsonify( {
         'hostname': socket.gethostname(),
         'timestamp': datetime.datetime.now().strftime("%I:%M:%S %p on %B %d, %Y"),
         'version': '1.0.0',
         'message': 'You are doing great, human! 😁👍',
-        'description': 'This is a simple Flask application that provides two API endpoints: /api/v1/details and /api/v1/healthz. The /api/v1/details endpoint returns information about the server, including the hostname, current timestamp, version, and a motivational message. The /api/v1/healthz endpoint returns the health status of the application along with the current timestamp.'
-    }
-    return jsonify(details)
+        'description': 'This is a simple Flask application that provides information about the server and its health status. It has two endpoints: one for retrieving server details and another for checking the health of the application.',
+        'deployed_on': 'kubernetes',
+    }), 200
 
 @app.route('/api/v1/healthz', methods=['GET'])
 def health_check():
-    health_status = {
+    return jsonify( {
         'status': 'healthy',
-        'timestamp': datetime.datetime.now().strftime("%I:%M%S %p on %B %d, %Y")
-    }
-    return jsonify(health_status), 200
+        'timestamp': datetime.datetime.now().strftime("%I:%M%S %p on %B %d, %Y"),
+    }), 200
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.getenv('PORT', 5000))
